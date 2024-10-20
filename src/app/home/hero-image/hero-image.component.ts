3d99ec236e969cbe2aa89f33f3a529e58cf9fd2e
Aidan Puricelli <aidanpuricelli@gmail.com>
2024-10-20 18:30:09 -0500

    parallax and header

--- src/app/home/hero-image/hero-image.component.ts
+++ src/app/home/hero-image/hero-image.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-hero-image',
   templateUrl: './hero-image.component.html',
-  styleUrl: './hero-image.component.css'
+  styleUrls: ['./hero-image.component.css']
 })
 export class HeroImageComponent {
 
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const heroContainer = document.querySelector('.hero-container') as HTMLElement;
+
+    // Horizontal movement: limited to a small range (45% - 55%)
+    const moveX = 45 + (event.clientX / window.innerWidth) * 2;
+    
+    // Vertical movement: limited to a smaller range (55% - 65%)
+    const moveY = 55 + (event.clientY / window.innerHeight) * 2;
+    
+    // Update the background position dynamically
+    heroContainer.style.backgroundPosition = `${moveX}% ${moveY}%`;
+  }
 }
